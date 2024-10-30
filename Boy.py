dc24a7912480dad294e82e340e7c69c2cf255f71
druv2000_laptop <richrichhong@gmail.com>
2024-10-30 17:40:54 +0900

    상태 다이어그램 추가

---
+++ Boy.py
@@ -0,0 +1,142 @@
+from pico2d import load_image, get_time
+
+from state_machine import *
+
+class Idle:
+   @staticmethod
+   def enter(boy, event):
+       if start(event):
+           boy.action = 3
+           boy.dir = 1
+       elif right_down(event) or left_up(event):
+           boy.action = 2
+           boy.dir = -1
+       elif left_down(event) or right_up(event):
+           boy.action = 3
+           boy.dir = 1
+
+       boy.start_time = get_time()
+       pass
+
+   @staticmethod
+   def exit(boy, event):
+       pass
+
+   @staticmethod
+   def do(boy):
+       boy.frame = (boy.frame + 1) % 8
+       if get_time() - boy.start_time > 0.5:
+           boy.state_machine.add_event(('TIME_OUT', 0))
+       pass
+
+   @staticmethod
+   def draw(boy):
+       boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+       pass
+
+
+
+class Sleep:
+   @staticmethod
+   def enter(boy, event):
+       boy.start_time = get_time()
+       pass
+
+   @staticmethod
+   def exit(boy, event):
+       pass
+
+   @staticmethod
+   def do(boy):
+       boy.frame = (boy.frame + 1) % 8
+       pass
+
+   @staticmethod
+   def draw(boy):
+       if boy.dir == 1:
+           boy.image.clip_composite_draw(
+               boy.frame * 100, boy.action * 100, 100, 100,
+               3.141592 / 2,
+               '',
+               boy.x, boy.y - 30, 100, 100
+           )
+       elif boy.dir == -1:
+           boy.image.clip_composite_draw(
+               boy.frame * 100, boy.action * 100, 100, 100,
+               -3.141592 / 2,
+               '',
+               boy.x, boy.y - 30, 100, 100
+           )
+
+class AutoRun:
+    @staticmethod
+    def enter(boy, event):
+        boy.start_time = get_time()
+        pass
+
+    @staticmethod
+    def exit(boy, event):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        pass
+
+    @staticmethod
+    def draw(boy):
+        pass
+
+
+
+class Run:
+    @staticmethod
+    def enter(boy, event):
+        if right_down(event) or left_up(event): # 오른쪽으로 RUN
+            boy.dir, boy.action = 1, 1
+        elif left_down(event) or right_up(event): # 왼쪽으로 RUN
+            boy.dir, boy.action = -1, 0
+    @staticmethod
+    def exit(boy, event):
+        pass
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.dir * 5
+        pass
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+
+
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.dir = 0
+        self.action = 3
+        self.image = load_image('animation_sheet.png')
+        self.state_machine = StateMachine(self)
+        self.state_machine.start(Idle)
+        self.state_machine.set_transitions(
+            {
+                Sleep:  {space_down: Idle, right_down: Run, left_down: Run, right_up: Run, left_up: Run},
+                Idle:   {time_out: Sleep, right_down: Run, left_down: Run, right_up: Run, left_up: Run},
+                Run:    {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle}
+            }
+        )
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        self.state_machine.add_event(
+            ('INPUT', event)
+        )
+        pass
+
+    def draw(self):
+        self.state_machine.draw()
---
+++ state_machine.py
@@ -0,0 +1,93 @@
+# event ( 이벤트 종류, 실제 값 )
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+
+def start(event):
+    return event[0] == 'START'
+
+def space_down(event):
+    return (event[0] == 'INPUT' and
+            event[1].type == SDL_KEYDOWN and
+            event[1].key == SDLK_SPACE)
+
+def time_out(event):
+    return event[0] == 'TIME_OUT'
+
+def right_down(event):
+    return (event[0] == 'INPUT' and
+            event[1].type == SDL_KEYDOWN and
+            event[1].key == SDLK_RIGHT)
+
+def right_up(event):
+    return (event[0] == 'INPUT' and
+            event[1].type == SDL_KEYUP and
+            event[1].key == SDLK_RIGHT)
+
+def left_down(event):
+    return (event[0] == 'INPUT' and
+            event[1].type == SDL_KEYDOWN and
+            event[1].key == SDLK_LEFT)
+
+def left_up(event):
+    return (event[0] == 'INPUT' and
+            event[1].type == SDL_KEYUP and
+            event[1].key == SDLK_LEFT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 상태 머신을 처리해주는 클래스
+class StateMachine:
+    def __init__(self, obj):
+        self.obj = obj
+        self.event_que = [] # 발생하는 이벤트(큐)
+        pass
+
+    def update(self):
+        self.cur_state.do(self.obj)
+        if self.event_que: # 리스트에 요소가 있으면
+            event = self.event_que.pop(0) # 리스트의 첫 번째 요소를 꺼냄
+            self.handle_event(event)
+
+
+    def start(self, start_state):
+        # 현재 상태를 시작 상태로 변경
+        self.cur_state = start_state
+        self.cur_state.enter(self.obj, ('START', 0))
+        print(f'ENTER into {self.cur_state}')
+        pass
+
+    def draw(self):
+        self.cur_state.draw(self.obj)
+        pass
+
+    def set_transitions(self, transitions):
+        self.transitions = transitions
+        pass
+
+    def add_event(self, event):
+        self.event_que.append(event) # 상태 머신용 이벤트 추가
+        print(f'    DEBUG: new event {event} is added.')
+        pass
+
+    def handle_event(self, event):
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(event):
+                self.cur_state.exit(self.obj, event)
+                print(f'EXIT from {self.cur_state}')
+                self.cur_state = next_state
+                self.cur_state.enter(self.obj, event)
+                print(f'ENTER into {next_state}')
+                return
